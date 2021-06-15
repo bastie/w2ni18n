658f921dcfca4f29f79f42ce8dbc1812f5db16e9
dimastro <d.mastrogiannopoulos@gmail.com>
2021-06-15 15:02:29 +0300

    Fix point char

--- python/word2numberi18n/w2n.py
+++ python/word2numberi18n/w2n.py
@@ -330,7 +330,7 @@ class W2N:
             if len(clean_decimal_numbers) > 0:
                 total_sum_as_string = str(result)+self.point_char+str(self.get_decimal_string(clean_decimal_numbers))
                 if lang != "gr":
-                result = float(total_sum_as_string)
+                    result = float(total_sum_as_string)
     
         return result
 
