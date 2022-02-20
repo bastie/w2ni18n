b732c4d9700bd188c6385136eed8e61168dfe60a
Sͬeͥbͭaͭsͤtͬian <bastie@users.noreply.github.com>
2022-02-20 12:31:08 +0100

    copy from python source and change comments

---
+++ Tests/w2ni18nTests/w2ni18nTests.swift
@@ -0,0 +1,16 @@
+/**
+ * SPDX-FileCopyrightText: 2022 - Sebastian Ritter <bastie@users.noreply.github.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+import XCTest
+@testable import w2ni18n
+
+final class w2ni18nTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(W2N().text, "Hello, World!")
+    }
+}
